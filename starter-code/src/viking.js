5675feef31fe205ec55bfc41f34929baa3fd6c29
agdwm <almudenaguerraswm@gmail.com>
2018-08-26 09:06:44 +0200

    First specification done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,17 @@
 // Soldier
-function Soldier() {}
+function Soldier (health, strength) {
+	this.health = health;
+	this.strength = strength;
+
+	this.receiveDamage = function(damage) {
+		this.health - damage;
+	}
+}
+
+Soldier.prototype.attack = function() {
+	return this.strength;
+}
+
 
 // Viking
 function Viking() {}
