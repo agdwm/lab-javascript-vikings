4e44f658b1715135fa73d114c03d23ab8f210c6f
agdwm <almudenaguerraswm@gmail.com>
2018-08-26 09:10:55 +0200

    Fixed function receiveDamage from the first specification

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -4,13 +4,18 @@ function Soldier (health, strength) {
 	this.strength = strength;
 
 	this.receiveDamage = function(damage) {
-		this.health - damage;
+		var currentHealth = this.health;
+		var newHealth = currentHealth - damage;
+		this.health = newHealth;
 	}
 }
 
 Soldier.prototype.attack = function() {
 	return this.strength;
 }
+
+var soldier1 = new Soldier(100, 50);
+soldier1.receiveDamage(10);
 
 
 // Viking
